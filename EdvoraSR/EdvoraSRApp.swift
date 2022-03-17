153f74c2b8b9ed2013296bafd2c565c341b76eb6
Prajjwal009 <sinhaprajjwal009@gmail.com>
2022-03-17 19:41:45 +0530

    Initial Commit

---
+++ EdvoraSR/EdvoraSRApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraSRApp.swift
+//  EdvoraSR
+//
+//  Created by Prajjwal on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraSRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
